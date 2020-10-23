7cccdac048fb5e1cefffadabf2896e0844d84164
Nicolas Trianta <nicolas.trianta@gmail.com>
2020-10-23 18:40:29 -0400

    facebook clone complete

--- fb-clone/src/index.js
+++ fb-clone/src/index.js
@@ -19,3 +19,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// Project Console: https://console.firebase.google.com/project/facebook-clone-e210f/overview
+// Hosting URL: https://facebook-clone-e210f.web.app
