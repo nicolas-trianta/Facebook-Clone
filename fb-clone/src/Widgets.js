92c41deea358e8509759895db8490ad410dae35f
Nicolas Trianta <nicolas.trianta@gmail.com>
2020-10-27 11:13:16 -0400

    changed iframe

--- fb-clone/src/Widgets.js
+++ fb-clone/src/Widgets.js
@@ -5,7 +5,8 @@ function Widgets() {
     return (
         <div className="widgets">
             <iframe 
-                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTipTopBrain%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1584959795107854" width="340" 
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1584959795107854" 
+                width="340" 
                 height="100%" 
                 style={{border: "none", overflow: "hidden"}}scrolling="no" 
                 frameborder="0" 
